8b14f5409918d19d4f92eb856d3e8c3d73142778
Romain <chaventre.romain@orange.fr>
2023-02-03 12:04:09 +0100

    mise en page banniere et header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Logo from '@/Images/Logo.jpg'
 
 import './header.css'
 
@@ -8,8 +9,10 @@ const Header = () => {
         <div className='Header'>
             <header>
                 <nav>
+                    <a href="/home">
+                        <img src={Logo} className="App-logo" alt="logo"></img>
+                    </a>
                     <ul>
-                        <a href="/home"> <img src='../Images/Logo.jpg' alt='logo'></img> </a>
                         <li><Link to="/home">Accueil</Link></li>
                         <li><Link to="/apropos">A propos</Link></li>
                     </ul>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Banniere from '@/Images/banniereacceuil.jpg'
+import './home.css'
 
 const Home = () => {
     return (
         <div>
             Accueil
+            <div className='Banniere'>
+                <img src={Banniere} className='banniereac' alt='bannierepdac'></img>
+                <p>Chez vous, partout et ailleurs</p>
+            </div>
         </div>
     );
 };
